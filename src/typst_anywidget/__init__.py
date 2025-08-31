643ef1e921ed0b331293a6ba2a95924230c66ed9
Mikael Reponen <mikael.reponen@protonmail.com>
2025-08-31 22:01:07 +0300

    prism-code-editor dependency and intial code commit

--- src/typst_anywidget/__init__.py
+++ src/typst_anywidget/__init__.py
@@ -10,7 +10,9 @@ except importlib.metadata.PackageNotFoundError:
     __version__ = "0.1"
 
 
-class Widget(anywidget.AnyWidget):
+class TypstInput(anywidget.AnyWidget):
     _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
     _css = pathlib.Path(__file__).parent / "static" / "widget.css"
-    value = traitlets.Int(0).tag(sync=True)
+    value = traitlets.Unicode("").tag(sync=True)
+    def __init__(self, value: value) -> None:
+          super().__init__(value=value)
