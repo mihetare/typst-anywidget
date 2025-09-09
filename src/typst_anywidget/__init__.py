4c8fa95f2075dc414daa0ab7adb3f429ada278be
Mikael Reponen <mikael.reponen@protonmail.com>
2025-09-09 23:31:08 +0300

    Starting move the typst compilation to a separate thread

--- src/typst_anywidget/__init__.py
+++ src/typst_anywidget/__init__.py
@@ -4,11 +4,22 @@ import anywidget
 import traitlets
 import typst
 import threading
-from tqdm.std import Bar
+import queue
+
 try:
     __version__ = importlib.metadata.version("typst_anywidget")
 except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.1"
+
+
+def typstThreadCompiler(inputQueue,outputQueue):
+    while True:
+        inputData = inputQueue.get()
+        try:
+            outputQueue.put(typst.compile(inputData[0]["new"].encode("utf-8"), format='svg', sys_inputs=inputData[1]))
+        except Exception as e:
+            outputQueue.put(f"Error: {str(e)}")
+        # inputQueue.task_done()
 
 class outputsvg_repr:
     def __init__(self, input):
@@ -25,24 +36,27 @@ class TypstInput(anywidget.AnyWidget):
     debounce = traitlets.Int(250).tag(sync=True)
     svgInput = traitlets.Unicode("").tag(sync=True)
     sysinput = traitlets.Dict({}).tag(sync=True)
-    op = None
+
     def __init__(self, value: str = "", debounce: int = 250, svgInput: str = "", sysinput: dict = {}) -> None:
         super().__init__(value=value, debounce=debounce, svgInput=svgInput, sysinput=sysinput)
         self.observe(self.compileTypst, names='value')
+        self.compilerThreads = []
+        self.inputQueue = queue.Queue()
+        self.outputQueue = queue.Queue()
+        self.compilerWorker = threading.Thread(target=typstThreadCompiler, args=(self.inputQueue,self.outputQueue))
+        self.compilerWorker.start()
+        self.op = None
 
     def setTypstInput(self, value):
         self.value = value
 
-    # def compileTypst(self):
-    #     self.op = typst.compile(self.value.encode("utf-8"), format='svg', sys_inputs=self.sysinput ) # sys_inputs=sys_inputs,
-    #     self.svgInput = self.op.decode('ASCII')
-
     def compileTypst(self, value):
         try:
             self.op = typst.compile(value["new"].encode("utf-8"), format='svg', sys_inputs=self.sysinput ) # sys_inputs=sys_inputs,
+            self.inputQueue.put([value, self.sysinput])
             self.svgInput = self.op.decode('ASCII')
         except Exception as e:
             print(f"Error compiling Typst: {e}")
+
     def getSvgRepr(self):
-        #self.compileTypst()
         return outputsvg_repr(self.op)
